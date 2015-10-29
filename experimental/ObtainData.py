82716af722b68fc7c3cc2eb4652803198949354d
Michael Tran <mike@airbears2-10-142-54-6.airbears2.1918.berkeley.edu>
2015-10-29 10:27:32 -0700

    Moved ObtainData to experimental

---
+++ experimental/ObtainData.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import nibabel as nib
+
+def pull3D(DictionaryOfVectors):
+	Three_D_Data = []
+	for key, value in DictionaryOfVectors.iteritems():
+		vector = value[0]
+		fileLocation = value[1]
+		img = nib.load(fileLocation)
+		data = img.get_data()
+		for i in range(0, len(vector)):
+			if vector[i] == 1:
+				Three_D_Data.append(data[:, :, :, i])
+	return Three_D_Data
+
+def ShowCortex(ListOf3dImages):
+	for image in ListOf3dImages:
+		zDimension = np.shape(image)[2]
+		for i in range(0, zDimension):
+			plt.imshow(image[:, :, i], cmap = "gray")
+			plt.show()
+
+# You'll have to guess the Z value from the images show in the previous function
+def generateEvenlySpacedZpoints(zValue):
+	return np.linspace(start = 0, stop = zValue, num = zValue)
