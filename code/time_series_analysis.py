1a94bf0e8b6e7aa0adfa9cbf150e927b403e208d
Michael Tran <mike@Michaels-MacBook-Pro.local>
2015-12-07 22:37:45 -0800

    Time Series on Raw Data

--- code/time_series_analysis.py
+++ code/time_series_analysis.py
@@ -15,9 +15,10 @@ from utils import diagnostics as diagnos
 from utils import get_object_neural as neural
 from utils import stimuli
 from utils import convolution as convol
-from utils import smooth as sm
 from utils import linear_model as lm
 from utils import maskfunc as msk
+import copy
+import statsmodels.api as sm
 
 # important path:
 
@@ -61,6 +62,64 @@ for key, img in sub_img.iteritems():
 print ("Complete!")
 print (separator)
 
+brain = sub_data["sub001_run001"]
+x = copy.deepcopy(sub_data)
+maskedDict, volDict = msk.generateMaskedBrain(x)
+s1r1Masked = maskedDict["sub001_run001"]
+s1r1Vol = volDict["sub001_run001"]
+
+brain = brain[:, 25:50, 32, :]
+s1r1Masked = s1r1Masked[:, 25:50, 32]
+brain = brain[s1r1Masked, :]
+
+arr = [0.0] * 121
+for i in range(121):
+	arr[i] = np.mean(brain[:, i])
+
+fig = plt.figure(figsize = (12,8))
+ax1 = fig.add_subplot(211)
+fig = sm.graphics.tsa.plot_acf(arr, lags = 20, ax = ax1)
+ax2 = fig.add_subplot(212)
+fig = sm.graphics.tsa.plot_pacf(arr, lags = 20, ax = ax2)
+#plt.show()
+plt.savefig("sub001_run001_corrFunc.png")
+plt.clf()
+plt.close()
+
+s1r1_arimaFit = sm.tsa.ARIMA(arr, (2, 0, 0)).fit()
+
+plt.gca().set_color_cycle(['red', 'blue'])
+plt.plot(arr)
+plt.plot(s1r1_arimaFit.fittedvalues)
+plt.legend(['Actual', 'Fitted'], loc='upper left')
+plt.title("Actual vs Fitted Time Series")
+plt.savefig("sub001_run001_TimeSeries.png")
+plt.clf()
+plt.close()
+#print("Printing ARIMA parameters")
+#print(s1r1_arimaFit.params)
+
+# Plot residuals of the fit here
+plt.plot(s1r1_arimaFit.resid)
+plt.title("Residuals of ARIMA Model on Run 1")
+plt.savefig("sub001_run001_residFit.png")
+plt.clf()
+plt.close()
+
+# print("P-Value for independence of residuals. If greater than 0.05, model is correct")
+# print(sm.stats.diagnostic.acorr_ljungbox(s1r1_arimaFit.resid, lags = 20))[1]
+
+fig = plt.figure(figsize = (12,8))
+ax1 = fig.add_subplot(211)
+fig = sm.graphics.tsa.plot_acf(s1r1_arimaFit.resid, lags = 20, ax = ax1)
+ax2 = fig.add_subplot(212)
+fig = sm.graphics.tsa.plot_pacf(s1r1_arimaFit.resid, lags = 20, ax = ax2)
+#plt.show()
+plt.savefig("sub001_run001_residcorrFunc.png")
+plt.clf()
+plt.close()
+
+# Focus on z = 32, y = 25 to 50, all of x
 
 # The sub_data is a dictionary for all 4D runs of this subject
 # The key for this dictionary are:
