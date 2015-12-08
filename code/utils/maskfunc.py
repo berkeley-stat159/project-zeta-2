155c0fd08cc64b65ff28eb16390ebc3ce21eeb36
Michael Tran <mike@Michaels-MacBook-Pro.local>
2015-12-07 23:44:00 -0800

    Changed unit test

--- code/utils/maskfunc.py
+++ code/utils/maskfunc.py
@@ -1,7 +1,5 @@
 import nibabel as nib
-import matplotlib.pyplot as plt
 import numpy as np
-import pickle
 
 # Takes in a dictionary with (K, V) pair (subxxx_runyyy, modified 4D Array)
 # Returns the masked with time collapsed and vol_mean
@@ -18,35 +16,6 @@ def generateMaskedBrain(allFiguresDict):
 		volumeDict[key] = vol_mean
 	return maskedDict, volumeDict
 
-#def generateMaskedBrain(paths):
-	#for path in paths:
-		#img = nib.load(path + '/filtered_func_data_mni.nii.gz')
-		#print(img.shape)
-		#data = img.get_data()
-
-		#vol_shape = data.shape[:-1]
-		#n_trs = data.shape[-1]
-		#print(vol_shape, n_trs)
-		#mean_vol = np.mean(data, axis = -1)
-		#percent = np.percentile(np.ravel(mean_vol), q = 80)
-		#print(percent)
-		#plt.hist(np.ravel(mean_vol), bins = 100)
-		#plt.show()
-
-		# in_brain_mask = mean_vol > percent
-
-		#data[mean_vol > percent, :] = True
-		#data[mean_vol < percent, :] = False
-		#print data[:,25,25,25]
-		#return data
-		# brainSave = save_obj(in_brain_tcs, 'test1')
-		# print(in_brain_tcs.shape)
-
-		# plt.imshow(mean_vol[:, :, 50])
-		# plt.show()
-		# plt.imshow(in_brain_mask[:, :, 50])
-		# plt.show()
-
 def generateMask(allFiguresDict, percent):
 	maskedDict = {}
 	volumeDict = {}
@@ -59,15 +28,6 @@ def generateMask(allFiguresDict, percent):
 		maskedDict[key] = vol_mean > percentile
 		volumeDict[key] = vol_mean
 	return maskedDict, volumeDict
-
-
-def save_obj(obj, name):
-	with open('/Users/mike/Downloads/ds105/sub001/model/model001/task001_run001.feat/' + name + '.pkl', 'wb') as f:
-		pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
-
-def load_obj(name):
-	with open('/Users/mike/Downloads/ds105/sub001/model/model001/task001_run001.feat/' + name + '.pkl', 'rb') as f:
-		return pickle.load(f)
 
 # generateMaskedBrain(['/Users/edithho/Downloads/ds105/sub001/model/model001/task001_run002.feat'])
 # test = load_obj('test1')
--- code/utils/tests/test_maskfunc.py
+++ code/utils/tests/test_maskfunc.py
@@ -1,19 +1,22 @@
 import numpy as np
 from .. import maskfunc as msk
-from .. import subject_class as sc
 from numpy.testing import assert_equal
 import copy
 
-def test_dictionary():
-	# which subject to work on?
-	subid = "sub001"
-	sub = sc.subject(subid)
-	sub_img = sub.run_img_result
-	sub_data = {}
-	for key, img in sub_img.iteritems():
-	    sub_data[key] = img.get_data()
-	x = copy.deepcopy(sub_data)
-	maskedDict, volDict = msk.generateMaskedBrain(x)
+def test_dictionary1():
+	x = range(16)
+	y = np.reshape(x, (2, 2, 2, 2))
+	newDict = {}
+	newDict["BugsBunny"] = y
+	maskedDict, volDict = msk.generateMaskedBrain(newDict)
+	assert_equal(len(maskedDict), len(volDict))
+
+def test_dictionary2():
+	x = range(16)
+	y = np.reshape(x, (2, 2, 2, 2))
+	newDict = {}
+	newDict["BugsBunny"] = y
+	maskedDict, volDict = msk.generateMask(newDict, 80)
 	assert_equal(len(maskedDict), len(volDict))
 
 
