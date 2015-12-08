e7ed23059d96bf21aacfa0ec64eacf50ce55915b
Michael Tran <mike@Michaels-MacBook-Pro.local>
2015-12-07 23:30:42 -0800

    Removed useless lines

--- code/utils/tests/test_maskfunc.py
+++ code/utils/tests/test_maskfunc.py
@@ -1,16 +1,8 @@
 import numpy as np
 from .. import maskfunc as msk
 from .. import subject_class as sc
-import os
 from numpy.testing import assert_equal
 import copy
-
-basepath = os.path.abspath(os.path.dirname(__file__))
-
-realpath = os.path.join(basepath, "..", "..", "..", "data", "ds105")
-realpath = os.path.join(realpath, "sub001", "model", "model001")
-realpath = os.path.join(realpath, "task001_run001.feat", "filtered_func_data_mni.nii.gz")
-dummypath = os.path.join(basepath, "testsub001run001.nii.gz")
 
 def test_dictionary():
 	# which subject to work on?
